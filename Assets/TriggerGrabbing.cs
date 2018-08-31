0ef63cccc5edb03f2d22dd92fb43098d58a79ea8
Marbelous <maksimikk@gmail.com>
2018-08-31 14:01:49 -0400

    Init Commit
    Holistic Climbing Tut.  Issue with the character ffalling down in the Y axis at beginning of the StandUpFromCrouch animation.

---
+++ Assets/TriggerGrabbing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGrabbing : MonoBehaviour
+{
+    public GameObject rootPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("TriggerGrabbing trigger entered.");
+        other.GetComponentInParent<ClimbUp>().GrabEdge(rootPos.transform);
+    }
+}
